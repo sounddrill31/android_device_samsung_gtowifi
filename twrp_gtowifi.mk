32b4c743f8092c79c848a2f42ca81b2137e8fb35
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-09-18 17:09:25 +0530

    Update twrp_gtowifi.mk

--- twrp_gtowifi.mk
+++ twrp_gtowifi.mk
@@ -21,7 +21,7 @@ PRODUCT_RELEASE_NAME := gtowifi
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
